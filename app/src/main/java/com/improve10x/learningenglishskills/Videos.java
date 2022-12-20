2182c3a241c80f9c440f796b53d8cf812d0d5451
Sumanth krishna M <sumanth.krishna@improve10x.com>
2022-12-20 16:01:23 +0530

    created a VideosActivity and its Xml and implemented videos java class

---
+++ app/src/main/java/com/improve10x/learningenglishskills/Videos.java
@@ -0,0 +1,10 @@
+package com.improve10x.learningenglishskills;
+
+public class Videos {
+    String imageUrl;
+    String title;
+    String channelName;
+    String numberOfViewers;
+    String logoImageUrl;
+    String uploadedTime;
+}
