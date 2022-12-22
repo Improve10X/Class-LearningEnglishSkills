6e0c160cabbc02bf67062cc45367967b5f97ad55
d pullaiah <pullaiah.dudekula@improve10x.com>
2022-12-22 11:36:30 +0530

    created a new playVideo activity xml

---
+++ app/src/main/java/com/improve10x/learningenglishskills/PlayVideoActivity.java
@@ -0,0 +1,15 @@
+package com.improve10x.learningenglishskills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_video);
+        getSupportActionBar().setTitle("Play Video");
+    }
+}
