10936042e1622b16934bf873d1797554a7edc68c
Suresh <suresh.sanganaboyana@improve10x.com>
2022-12-23 12:16:36 +0530

    implemented view binding in PlayVideoActivity

--- app/src/main/java/com/improve10x/learningenglishskills/youtubeVideos/PlayVideoActivity.java
+++ app/src/main/java/com/improve10x/learningenglishskills/youtubeVideos/PlayVideoActivity.java
@@ -12,32 +12,28 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.improve10x.learningenglishskills.Constants;
 import com.improve10x.learningenglishskills.R;
+import com.improve10x.learningenglishskills.databinding.ActivityPlayVideoBinding;
 import com.squareup.picasso.Picasso;
 
 public class PlayVideoActivity extends YouTubeBaseActivity {
 
+    private ActivityPlayVideoBinding binding;
     private Video video;
-    private YouTubePlayerView youTubePlayerView;
-    private ImageView videoLogoImageUrl;
-    private TextView videoTitleText;
-    private TextView videoChannelNameTxt;
-    private TextView videoViewsTxt;
-    private TextView videoUploadedDateTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_video);
+        binding = ActivityPlayVideoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Intent intent = getIntent();
         intent.hasExtra(Constants.KEY_LEARNING_ENGLISH_SKILLS);
         video = (Video) intent.getSerializableExtra(Constants.KEY_LEARNING_ENGLISH_SKILLS);
-        setupViews();
         showData();
         setupYouTubePlayerView();
     }
 
     private void setupYouTubePlayerView() {
-        youTubePlayerView.initialize(Constants.YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+        binding.youtubePlayerView.initialize(Constants.YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 youTubePlayer.loadVideo(video.youtubeVideoId);
@@ -47,26 +43,15 @@ public class PlayVideoActivity extends YouTubeBaseActivity {
             @Override
             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
                 Toast.makeText(PlayVideoActivity.this, "Fail to load video", Toast.LENGTH_SHORT).show();
-
             }
         });
     }
 
-
     private void showData() {
-        Picasso.get().load(video.logoImageUrl).into(videoLogoImageUrl);
-        videoTitleText.setText(video.title);
-        videoChannelNameTxt.setText(video.channelName);
-        videoViewsTxt.setText(video.numberOfViews);
-        videoUploadedDateTxt.setText(video.uploadedDate);
-    }
-
-    private void setupViews() {
-        youTubePlayerView = findViewById(R.id.youtube_player_view);
-        videoLogoImageUrl = findViewById(R.id.video_logo_image_url);
-        videoTitleText = findViewById(R.id.video_title_txt);
-        videoChannelNameTxt = findViewById(R.id.video_channel_name_txt);
-        videoViewsTxt = findViewById(R.id.video_views_txt);
-        videoUploadedDateTxt = findViewById(R.id.video_upload_date_txt);
+        Picasso.get().load(video.logoImageUrl).into(binding.videoLogoImageUrl);
+        binding.videoTitleTxt.setText(video.title);
+        binding.videoChannelNameTxt.setText(video.channelName);
+        binding.videoViewsTxt.setText(video.numberOfViews);
+        binding.videoUploadDateTxt.setText(video.uploadedDate);
     }
 }
